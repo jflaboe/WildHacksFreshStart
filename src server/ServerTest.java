316fa25611cee44ff3994f6a29242420a252f165
John Laboe <John Laboe>
2016-11-19 16:45:35 -0600

    multiple node functionality

--- src server/ServerTest.java
+++ src server/ServerTest.java
@@ -1,9 +1,28 @@
-import javax.swing.*;
 
-public class ServerTest {
-	public static void main(String args[]) {
-		Server s = new Server();
-		s.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		s.startRunning();
-	}
+
+public class ServerTest{
+    
+    
+    
+    
+    public ServerTest()
+    {
+        // Title
+        
+        
+        
+		
+		
+		
+    }
+    
+    public static void main(String args[]) {
+        
+        ServerFrame chatbox = new ServerFrame();
+        ServerClient initialServerClient = new ServerClient(chatbox);
+        
+        
+        initialServerClient.run();
+    }
+   
 }
